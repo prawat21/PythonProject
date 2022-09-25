566d10d8d89c0e0cde6f227310149d15dc98ee50
Puneet Kumar Rawat <prawat@automotivemastermind.com>
2022-09-25 16:07:22 +0530

    firstCommit

---
+++ Config/config.py
@@ -0,0 +1,8 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "C:/Users/prawat/Downloads/chromedriver/chromedriver.exe"
+    FIREFOX_EXECUTABLE_PATH = "C:/Users/prawat/Downloads/geckodriver.exe"
+
+    BASE_URL = "https://app.hubspot.com/login"
+    USER_NAME = "puneetrawat852@gmail.com"
+    PASSWORD = "Selenium@12345"
+    LOGIN_PAGE_TITLE = "HubSpot Login"
