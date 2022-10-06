b0d06626ed3288f0db1534e0318e2fa88da7b2b8
Puneet Kumar Rawat <prawat@automotivemastermind.com>
2022-10-06 14:06:03 +0530

    fixerror

--- PytestSessions/test_params.py
+++ PytestSessions/test_params.py
@@ -1,21 +1,22 @@
+import time
+
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.usefixtures("init_driver")
-class BaseTest():
+@pytest.mark.usefixtures("init__driver")
+class BaseTest:
     pass
 
 
 class TestHubSpot(BaseTest):
-
     @pytest.mark.parametrize(
         "username, password",
         [
-            ("admin@gmail.com", "admin123"),
-            ("puneetrawat@gmail.com", "puneet123"),
+            #  ("admin@gmail.com", "admin123"),
+            ("puneetrawat852@gmail.com", "Selenium@12345"),
         ]
 
     )
@@ -28,4 +29,6 @@ class TestHubSpot(BaseTest):
         """
         self.driver.get("https://app.hubspot.com/login")
         self.driver.find_element(By.ID, 'username').send_keys(username)
+        time.sleep(3)
         self.driver.find_element(By.ID, 'password').send_keys(password)
+        time.sleep(3)
